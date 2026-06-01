e931540494cd081b1dce79d4ff263021fe091b65
dikey4405 <167569079+dikey4405@users.noreply.github.com>
2026-06-01 22:48:19 +0700

    Add solution for anchor-box-generation

---
+++ anchor-box-generation/anchor-box-generation.py
@@ -0,0 +1,21 @@
+import numpy as np
+def generate_anchors(feature_size, image_size, scales, aspect_ratios):
+    """
+    Generate anchor boxes for object detection.
+    """
+    # Write code here
+    stride = image_size / feature_size
+    anchor = []
+    for i in range(feature_size):
+        for j in range(feature_size):
+            cx = (j+0.5)*stride
+            cy = (i+0.5)*stride
+
+            for s in scales:
+                for r in aspect_ratios:
+                    w = s * np.sqrt(r)
+                    h = s / np.sqrt(r)
+
+                    anchor.append([cx - w/2, cy - h/2, cx + w/2, cy + h/2])
+    return anchor
+        
